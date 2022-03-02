f9f73674afd908f61e5bd9f291ead6ecfd374d99
Paulodiastst <paulodiastst@hotmail.com>
2022-03-01 21:26:31 -0300

    Fazendo o CRUD

---
+++ src/main/java/br/com/feltex/academicnet/modelo/Aluno.java
@@ -0,0 +1,25 @@
+package br.com.feltex.academicnet.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.Instant;
+
+
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@Data
+public class Aluno {
+
+    @Id
+    private Long matricula;
+    private String nome;
+    private String telefone;
+    private String email;
+    private Instant dataCadastro;
+
+}
